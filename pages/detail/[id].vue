09c6544b4b7078a9448cba1dbbde0c60b3899489
Lucas Mendonça <lucassms9@hotmail.com>
2023-03-04 01:33:02 -0300

    feat: page detail

---
+++ pages/detail/[id].vue
@@ -0,0 +1,11 @@
+<script setup>
+const route = useRoute()
+// When accessing /posts/1, route.params.id will be 1
+console.log(route.params.id)
+</script>
+
+<template>
+    <section>
+        <p>This page will be displayed at the /about route.</p>
+    </section>
+</template>
