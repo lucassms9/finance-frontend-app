7eaa43b3694572408ae17e8069e6637c23b44a39
Lucas Mendonça <lucassms9@hotmail.com>
2023-03-04 01:32:30 -0300

    feat: calendar component

---
+++ components/Calendar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <label for="first_name" class="block mb-2 text-sm font-medium">Starting Month</label>
+        <VueDatePicker hide-input-icon v-model="date" month-picker placeholder="yyyy-mm" class="bg-gray-50"></VueDatePicker>
+    </div>
+</template>
+  
+<script>
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+export default {
+    components: { VueDatePicker },
+    data() {
+        return {
+            date: null,
+        };
+    }
+}
+</script>
+  
