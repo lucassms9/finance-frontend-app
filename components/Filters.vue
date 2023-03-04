43f3dd6f9d4f875c05705eb4965683441e1ba1e3
Lucas Mendonça <lucassms9@hotmail.com>
2023-03-04 01:32:54 -0300

    feat: filter component

---
+++ components/Filters.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex mb-2 gap-4 ">
+        <div class="w-2/4"> <Input /></div>
+        <div class="w-auto"> <Select /></div>
+        <div class="w-auto"> <Select /></div>
+        <div class="w-auto"> <Calendar /></div>
+        <div class="w-auto"> <Calendar /></div>
+    </div>
+</template>
